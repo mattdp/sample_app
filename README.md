c5f0d28984b70629d59982a2fd1fc730a7323b9f
Matthew Du Pont <dupont@okcupidlabs.com>
2012-07-25 19:38:42 -0700

    Initial commit, round two

---
+++ README.md
@@ -0,0 +1,3 @@
+## RoR Tutorial: sample application
+
+This is the sample application from [Michael Hartl's tutorial](http://railstutorial.org)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '62eacc91d4ea90f4b7e1343b3baf90dd1bb8ca70aa299377f3cb1efe954594499f7e1fd1a780ab71cff19a22e55fa375870e2c27ee47159071aef7df6613576d'
