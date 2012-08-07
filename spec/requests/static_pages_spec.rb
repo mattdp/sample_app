7c87083f1d7d06a556a4dd78c3bf167a9040bfd2
Matthew Du Pont <dupont@okcupidlabs.com>
2012-08-06 20:17:43 -0700

    basic routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,45 +3,45 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
     it "should have a title in ... | Home format" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => " | Home")
     end
   end
   
   describe "Help page" do
     it "should be 'Help'ful" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     it "should have a ' | Help' title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => " | Help")
     end
   end
   
   describe "About page" do
     it "should be 'About' stuff" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About')
     end
     it "should have an ' | About' title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => " | About")
     end
   end
   
   describe "Contact page" do
     it "should have 'contact' in the h1" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'contact')
     end
     
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', text: 'Contact')
     end
   end  
