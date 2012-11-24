89d68b9ee5907bfcfd3f407b04acf6c1cc1c9d76
Matthew Du Pont <dupont@okcupidlabs.com>
2012-11-23 19:18:28 -0800

    Finish signin

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,6 +20,12 @@ describe "AuthenticationPages" do
 
   		it { should have_selector('title', text: "Sign in") }
   		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before {click_link "Home"}
+        it {should_not have_selector('div.alert.alert-error', text: 'Invalid') }
+      end
+
   	end
 
   	describe "with valid information" do
@@ -34,7 +40,14 @@ describe "AuthenticationPages" do
   		it { should have_link('Profile', href: user_path(user)) }
   		it { should have_link('Sign out', href: signout_path) }
   		it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before {click_link "Sign out"}
+        it {should have_link('Sign in') }
+      end
+
   	end
+
   end
 
 end
